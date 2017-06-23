ac0cdcd29310ee7b82af827eddc00dfa0fdd87e7
uplus <uplus.e10@gmail.com>
2017-06-23 20:21:52 +0900

    Add flash

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {name:  "",
+                                       email: "user@invalid",
+                                       password:              "foo",
+                                       password_confirmation: "bar"} }
+    end
+    assert_template 'users/new'
+
+    assert_select 'div#error_explanation' do
+      assert_select 'ul/li', count: 4
+      assert_select 'div.alert-danger', 'The form contains 4 errors.'
+    end
+    assert_select 'div.field_with_errors'
+  end
+end
